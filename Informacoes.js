52fab13a15dd8adb73051d61f0362fab5b729eaa
Benedito Corrêa <150083286+BacoDevSys@users.noreply.github.com>
2023-11-07 19:42:39 -0300

    Create Informacoes.js

---
+++ Informacoes.js
@@ -0,0 +1,5 @@
+document.getElementById('current-date').textContent = getCurrentDate();
+function toggleMenu() {
+    var menu = document.querySelector('.menu');
+    menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
+}
